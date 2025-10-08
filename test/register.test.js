93fc01939fee1f387a582cd8a92e955b51ed9758
Ravella Rodrigues <thuanyravella@gmail.com>
2025-10-08 01:36:06 -0400

    alterando o arquivo register.test.js para gerar valor unico de username e utilizar a fixture de register

--- test/register.test.js
+++ test/register.test.js
@@ -1,24 +1,23 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const { generateUniqueUsername } = require('../helpers/newUsers');
+const postRegister = require('../fixtures/postRegister.json');
 
 require('dotenv').config();
 
 describe('Users', () => {
     describe('POST /register', () => {
         it('Deve retornar sucesso com 201 - Usuário Criado - quando informo valores válidos para registro de novo usuário', async () => {
+            const bodyRegister = { ...postRegister };
+            bodyRegister.username = generateUniqueUsername();
+
             const response = await request(process.env.BASE_URL)
                 .post('/users/register')
                 .set('Content-Type', 'application/json')
-                .send({
-                    username: "alice.almeida",
-                    password: "123456",
-                    favorecidos: [
-                        ""
-                    ]
-                })
-
+                .send(bodyRegister);
+            
             expect(response.status).to.equal(201);
-            expect(response.body.username).to.equal('alice.almeida');
+            expect(response.body.username).to.equal(bodyRegister.username);
             expect(response.body.favorecidos).to.be.an('array');
             expect(response.body.favorecidos).to.deep.equal(['']);
         })
