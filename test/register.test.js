74d96e28e5caf101d618a4a2951f63a017c6ab50
Ravella Rodrigues <thuanyravella@gmail.com>
2025-10-08 22:32:10 -0400

    adicionando teste de usuário já registrado

--- test/register.test.js
+++ test/register.test.js
@@ -7,9 +7,11 @@ require('dotenv').config();
 
 describe('Users', () => {
     describe('POST /register', () => {
+        const usernameRegister = generateUniqueUsername();
+
         it('Deve retornar sucesso com 201 - Usuário Criado - quando informo valores válidos para registro de novo usuário', async () => {
             const bodyRegister = { ...postRegister };
-            bodyRegister.username = generateUniqueUsername();
+            bodyRegister.username = usernameRegister;
 
             const response = await request(process.env.BASE_URL)
                 .post('/users/register')
@@ -22,6 +24,19 @@ describe('Users', () => {
             expect(response.body.favorecidos).to.deep.equal(['']);
             expect(response.body.saldo).to.be.a('number');
             expect(response.body.saldo).to.equal(10000);
+        })
+
+        it('Deve retornar 400 - Usuário já existe - quando informo valores de um usuário que já foi registrado anteriormente', async () => {
+            const bodyRegister = { ...postRegister };
+            bodyRegister.username = usernameRegister;
+
+            const response = await request(process.env.BASE_URL)
+                .post('/users/register')
+                .set('Content-Type', 'application/json')
+                .send(bodyRegister);
+            
+            expect(response.status).to.equal(400);
+            expect(response.body.error).to.equal('Usuário já existe');
         })
     })
 })
