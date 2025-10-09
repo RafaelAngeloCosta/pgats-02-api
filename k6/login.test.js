8af318485fce6ed8f3f473075ca8653eae3e2ad2
Sidinei <sydney.ramos@hotmail.com>
2025-10-08 21:01:19 -0300

    adicionando os testes de API e performance no login

---
+++ k6/login.test.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { pegarBaseURL } from '../utils/variaveis.js';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 10 },
+        { duration: '30s', target: 20 },
+        { duration: '20s', target: 15 },
+        { duration: '10s', target: 0 }
+    ],
+    thresholds: {
+        http_req_duration: ['p(90)<3000', 'max<5000'],
+        http_req_failed: ['rate<0.01']
+    }
+};
+
+export default function () {
+    const url = pegarBaseURL() + '/users/login';
+
+    const payload = JSON.stringify(postLogin);
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+
+    check(res, {
+        'Validar que o Status é 200': (r) => r.status === 200,
+        'Validar que o Token é string': (r) => typeof(r.json().token) === 'string'
+    })
+
+    sleep(1);
+}
---
+++ test/login.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const { expect } = require('chai');
+require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json')
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token em string quando usar credencias válidas', async () => {
+            const bodyLogin = { ...postLogin}
+            
+            const resposta = await request(process.env.BASE_URL)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin) 
+                      
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.token).to.be.a('string');
+        })
+
+        it('Deve retornar 400 quando parametros de login usuário ou senha inválidos', async () => {
+            const bodyLogin = { ...postLogin}
+            bodyLogin.username = 'julio.lima'
+            bodyLogin.password = '987654'
+            
+            const resposta = await request(process.env.BASE_URL)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)            
+            expect(resposta.status).to.equal(400);
+        })
+    })
+})
